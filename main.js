02355591a90e9ec688f766db34deb1f365ab8a47
chojeongeun <juy0816@naver.com>
2023-10-08 10:07:51 +0900

    시간차 옵션 추가

--- main.js
+++ main.js
@@ -9,12 +9,18 @@ h1.innerHTML = tags;
 
 //h1 요소는 자동적으로 분리해서 span으로 감싸도록 자동처리하는 방법
 //el -> 선택자가 들어가는 요소는 함수명을 el, elem 등으로 써주는게 좋음
-function splitText(el, tagName = 'span') {
+function splitText(el, interval = 0, tagName = 'span') {
 	const text = el.innerText;
 	el.innerHTML = '';
 	let tags = '';
-	for (let letter of text) tags += `<${tagName} style='display:inline-block'>${letter}</${tagName}>`;
+	let count = 0;
+
+	for (let letter of text) {
+		tags += `<${tagName} style='display:inline-block; transition-delay:${interval * count}s'>${letter}</${tagName}>`;
+		count++;
+	}
+
 	el.innerHTML = tags;
 }
 
-splitText(h1);
+splitText(h1, 0.2);
